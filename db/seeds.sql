69e940008d84a383c7929271edcf95481dda37a8
buzzhaha <boming.fan@gmail.com>
2018-01-17 10:06:42 -0800

    start

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(burger_name) VALUES ("Cheese Burger"), ("Chicken Burger"), ("Beef Burger");
+INSERT INTO burgers(burger_name, devoured) VALUES ("Big Mac", true), ("Filet-O-Fish", true);
