915d394a2d6be41b7659ee8940b40c1d5524f8eb
buzzhaha <boming.fan@gmail.com>
2018-01-19 10:31:42 -0800

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 var mysql = require("mysql");
-
+var connection;
 // mysql connection
 
-var connection = mysql.createConnection({
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { 
+  connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "19831030",
     database: "burgers_db"
   });
-
+}
 // Make connection.
 connection.connect(function(err) {
     if (err) {
