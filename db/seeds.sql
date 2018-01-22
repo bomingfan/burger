829bee163cc794679a723ee54b5dabe5a3ef8d0e
buzzhaha <boming.fan@gmail.com>
2018-01-22 09:24:52 -0800

    layout

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
+
+use snll7i7cyajz4u3l;
+
 INSERT INTO burgers(burger_name) VALUES ("Cheese Burger"), ("Chicken Burger"), ("Beef Burger");
 INSERT INTO burgers(burger_name, devoured) VALUES ("Big Mac", true), ("Filet-O-Fish", true);
--- db/test.sql
+++ db/test.sql
@@ -1,2 +1,9 @@
-use burgers_db;
-select * from burgers;
+use snll7i7cyajz4u3l;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255),
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
